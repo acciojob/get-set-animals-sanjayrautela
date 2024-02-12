6ad0acc30e9e4e0d22fb349f63a75fdb178fa956
Sanjay Rautela <140192191+sanjayrautela@users.noreply.github.com>
2024-02-12 10:34:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-//complete this code
-class Animal {
-	constructor(species){
-		this._species = species;
+	//complete this code
+	class Animal {
+		constructor(species){
+			this._species = species;
+		}
+	
+		get species(){
+			return this._species;
+		}
+	
+		makeSound(){
+			console.log(`The ${this.species} makes a sound`);
+		}
 	}
-
-	get species(){
-		return this._species;
+	
+	class Dog extends Animal {
+		bark(){
+			console.log("wolf");
+		}
 	}
-
-	makeSound(){
-		console.log(`The ${species} makes a sound`);
+	
+	class Cat extends Animal {
+		purr(){
+			console.log("purr");
+		}
 	}
-}
-
-class Dog extends Animal {
-	bark(){
-		console.log("wolf");
-	}
-}
-
-class Cat extends Animal {
-	purr(){
-		console.log("purr");
-	}
-}
-
-// Do not change the code below this line
-window.Animal = Animal;
-window.Dog = Dog;
-window.Cat = Cat;
+	
+	// Do not change the code below this line
+	window.Animal = Animal;
+	window.Dog = Dog;
+	window.Cat = Cat;
